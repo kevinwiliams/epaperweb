527439da27890e25faf365b9578eb2e928c6fec8
Kevin Williams <kwilliams9285@gmail.com>
2022-10-27 02:24:15 -0500

    API (work In progress)

--- Controllers/ReaderController.cs
+++ Controllers/ReaderController.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using ePaperWeb.DBModel;
+using ePaperWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace ePaperWeb.Controllers
 {
     public class ReaderController : ApiController
     {
+        [HttpPost]
+        [ResponseType(typeof(Reader))]
+        public HttpResponseMessage Post([FromBody] Reader reader)
+        {
+            if (ModelState.IsValid && reader != null)
+            {
+                MemberData mb = new MemberData();
+
+                // Convert any HTML markup in the status text.
+                reader.call = HttpUtility.HtmlEncode(reader.call);
+                using (var context = new Entities())
+                {
+                    var subscriber = context.subscribers.SingleOrDefault(b => b.emailAddress == reader.username);
+                    if (subscriber != null) {
+                        mb.userID = subscriber.subscriberID;
+                        mb.loginName = subscriber.emailAddress;
+                        mb.firstname = subscriber.firstName;
+                        mb.lastname = subscriber.lastName;
+
+                    }
+                }
+
+                //var response = new HttpResponseMessage(HttpStatusCode.Created);
+                var response =  Request.CreateResponse(HttpStatusCode.Created, mb);
+
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            // return reader.call;
+        }
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+
+        [HttpPost]
+        public List<string> Get(int Id)
+        {
+            return new List<string> {
+                "Data1",
+                "Data2"
+            };
+        }
     }
 }
---
+++ Models/Reader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ePaperWeb.Models
+{
+    public class Reader
+    {
+        public string call { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int userid { get; set; }
+        public string token { get; set; }
+    }
+
+    public class MemberData
+    {
+        public int userID { get; set; }
+        public string loginName { get; set; }
+        public string email { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+}
